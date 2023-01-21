87e3f1fc1b2dd194926241a530f17ae9ea72bc6a
Kirill <kirillbronski@gmail.com>
2023-01-21 23:51:12 +0300

    second realization countdown timer. difference between collect and collectLatest

--- app/src/main/java/com/bronski/kotlinflows/MainViewModel.kt
+++ app/src/main/java/com/bronski/kotlinflows/MainViewModel.kt
@@ -1,8 +1,12 @@
 package com.bronski.kotlinflows
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
 
@@ -10,10 +14,27 @@ class MainViewModel : ViewModel() {
         val startingValue = 10
         var currentValue = startingValue
         emit(currentValue)
-        while (currentValue > 0){
+        while (currentValue > 0) {
             delay(1000)
             currentValue--
             emit(currentValue)
+        }
+    }
+
+    init {
+        collectFlow()
+    }
+
+    private fun collectFlow() {
+        viewModelScope.launch(Dispatchers.IO) {
+            countDownFlow.collect {
+                delay(1500)
+                println("The current time is $it")
+            }
+            countDownFlow.collectLatest {
+                delay(1500)
+                println("CollectLatest - The current time is $it")
+            }
         }
     }
 
