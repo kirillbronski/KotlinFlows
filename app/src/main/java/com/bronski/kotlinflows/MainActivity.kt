7283331d36c136ea2bb7e3aa7afdd875725f1c39
Kirill <kirillbronski@gmail.com>
2023-01-21 23:37:13 +0300

    first realization countdown timer

--- app/src/main/java/com/bronski/kotlinflows/MainActivity.kt
+++ app/src/main/java/com/bronski/kotlinflows/MainActivity.kt
@@ -2,10 +2,31 @@ package com.bronski.kotlinflows
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
+import com.bronski.kotlinflows.databinding.ActivityMainBinding
+import kotlinx.coroutines.flow.collectLatest
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
+    private val viewModel = MainViewModel()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(_binding!!.root)
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.countDownFlow.collectLatest {
+                binding.tvCountdownTimer.text = it.toString()
+            }
+        }
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
--- app/src/main/java/com/bronski/kotlinflows/MainViewModel.kt
+++ app/src/main/java/com/bronski/kotlinflows/MainViewModel.kt
@@ -1,7 +1,20 @@
 package com.bronski.kotlinflows
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.flow
 
 class MainViewModel : ViewModel() {
+
+    val countDownFlow = flow {
+        val startingValue = 10
+        var currentValue = startingValue
+        emit(currentValue)
+        while (currentValue > 0){
+            delay(1000)
+            currentValue--
+            emit(currentValue)
+        }
+    }
 
 }
